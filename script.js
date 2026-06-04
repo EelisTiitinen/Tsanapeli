7eec56ade21fdfff82f0b2ccf1d555b354c7e045
Eelis Tiitinen <53115@edu.raseko.fi>
2026-06-04 12:46:26 +0300

    pääominaisuudet toimii

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ var kysymykset = [];
 
 const kysymys = document.getElementById("kysymys");
 
-const nappiA = document.getElementById("a-btn");
-const nappiB = document.getElementById("b-btn");
-const nappiC = document.getElementById("c-btn");
-const nappiD = document.getElementById("d-btn");
+const napit = document.getElementById("napit");
+
+const tempNapit = napit.innerHTML;
 
 var randKys;
 var randKysIndex = -1;
+
+var oikeat = 0;
 
 fetch("kysymykset.json")
     .then(function(response) {
@@ -17,36 +18,47 @@ fetch("kysymykset.json")
     .then(function(data) {
         kysymykset = data;
         uusiKysymys();
-    })
-    .catch(function(error) {
-        console.error("Kysymysten lataaminen ep\u00e4onnistui:", error);
-        kysymys.innerHTML = "Kysymyksi\u00e4 ei voitu ladata.";
     });
 
 function uusiKysymys() {
-    var uusiIndex = Math.floor(Math.random() * kysymykset.length);
+    randKysIndex ++;
 
-    while (kysymykset.length > 1 && uusiIndex == randKysIndex) {
-        uusiIndex = Math.floor(Math.random() * kysymykset.length);
-    }
-
-    randKysIndex = uusiIndex;
     randKys = kysymykset[randKysIndex];
 
-    kysymys.innerHTML = randKys.kysymys
+    napit.innerHTML = tempNapit;
+    
+    const nappiA = document.getElementById("a-btn");
+    const nappiB = document.getElementById("b-btn");
+    const nappiC = document.getElementById("c-btn");
+    const nappiD = document.getElementById("d-btn");
 
-    nappiA.innerHTML = "A. " +  randKys.A
-    nappiB.innerHTML = "B. " +  randKys.B
-    nappiC.innerHTML = "C. " +  randKys.C
-    nappiD.innerHTML = "D. " +  randKys.D
+    kysymys.innerHTML = randKys.kysymys;
+
+    nappiA.innerHTML = "A. " +  randKys.A;
+    nappiB.innerHTML = "B. " +  randKys.B;
+    nappiC.innerHTML = "C. " +  randKys.C;
+    nappiD.innerHTML = "D. " +  randKys.D;
+
+    kysymys.style.color = "black";
 }
 
 function tarkista(arvaus) {
-    if (!randKys) return;
-
     if (arvaus == randKys.oikea) {
-        alert("Oikein!")
-        uusiKysymys();
+        kysymys.innerHTML = "Oikein!";
+        kysymys.style.color = "green";
+        oikeat ++;
     }
-    else alert("Väärin!")
+    else {
+        kysymys.innerHTML = "Väärin!";
+        kysymys.style.color = "red";
+    }
+
+    
+    if (randKysIndex == kysymykset.length - 1) {
+        kysymys.innerHTML = "Sait " + oikeat + "/" + kysymykset.length + " oikein!";
+        napit.innerHTML = "";
+    }
+    else {
+        napit.innerHTML = '<button onclick="uusiKysymys()">Seuraava</button>';
+    }
 }
